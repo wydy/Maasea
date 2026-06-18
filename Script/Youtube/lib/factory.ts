94b34e99834c3343974cb9beeb1f6d992a523815
Wydy <1937986+wydy@users.noreply.github.com>
2026-06-19 00:10:55 +0800

    Revert "Update"
    This reverts commit db6022dce8274949315f50cf2b943fa7c989a9c1.

--- Script/Youtube/lib/factory.ts
+++ Script/Youtube/lib/factory.ts
@@ -1,24 +1,20 @@
 import {
   BrowseMessage,
   NextMessage,
-  PlayerMessage,
   SearchMessage,
   ShortsMessage,
   GuideMessage,
-  SettingMessage,
-  WatchMessage
+  SettingMessage
 } from '../src/responseHandler'
 import { YouTubeMessage } from '../src/youtube'
 
 const messages = new Map([
   ['browse', BrowseMessage],
   ['next', NextMessage],
-  ['player', PlayerMessage],
   ['search', SearchMessage],
   ['reel_watch_sequence', ShortsMessage],
   ['guide', GuideMessage],
-  ['get_setting', SettingMessage],
-  ['get_watch', WatchMessage]
+  ['get_setting', SettingMessage]
 ])
 
 export default function createMessage (url): YouTubeMessage | null {
